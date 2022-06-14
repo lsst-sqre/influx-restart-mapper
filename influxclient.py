ecaa09181577c7b96414b506deb7190d3f669ce6
adam <athornton@lsst.org>
2022-06-14 11:42:55 -0700

    WIP

--- influxclient.py
+++ influxclient.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+import asyncio
 import logging
 import os
 import aiohttp
+
+from typing import Any, Dict, List
 
 # InfluxDB2 defaults
 
 DEFAULT_ORG = "default"
 DEFAULT_URL = "http://localhost:8086"
+
 
 class InfluxClient:
     def __init__(
@@ -30,8 +34,12 @@ class InfluxClient:
         self.api_url = url + "/api/v2"
         self.params = {"org": self.org}
         self.session = aiohttp.ClientSession()
-        self.session.headers.update({"Authorization": f"Token {token}",
-                                     "Content-Type": "application/json"})
+        self.session.headers.update(
+            {
+                "Authorization": f"Token {token}",
+                "Content-Type": "application/json",
+            }
+        )
 
     async def __aenter__(self):
         return self
@@ -39,3 +47,60 @@ class InfluxClient:
     async def __aexit__(self, *excinfo):
         if self.session:
             await self.session.close()
+
+    async def get(
+        self,
+        url: str,
+        params: Dict[str, str] = {},
+        use_session_params: bool = True,
+    ) -> Dict[str, Any]:
+        if use_session_params:
+            params.update(self.params)
+        self.log.debug(f"HTTP GET -> {url}, params=[{params}]")
+        resp = await self.session.get(url, params=params)
+        obj = await resp.json()
+        self.log.debug(f"HTTP GET Response -> {obj}")
+        return obj
+
+    async def post(
+        self,
+        url: str,
+        payloads: List[Dict[str, Any]],
+        params: Dict[str, str] = {},
+        use_session_params: bool = True,
+    ) -> List[Any]:
+        params = self.params
+        if not use_session_params:
+            params = {}
+        if not payloads:
+            return []
+        self.log.debug(
+            f"HTTP POST -> {url}, params=[{params}], "
+            + f"first body = {payloads[0]}"
+        )
+        payload_futs = [
+            self.session.post(url, json=x, params=params) for x in payloads
+        ]
+        resps = await asyncio.gather(*payload_futs)
+        self.log.debug(f"HTTP POST Responses -> {resps}")
+        return resps
+
+    async def list_all(
+        self, url: str, itemtype: str, pagesize: int = 20
+    ) -> List[Dict[str, Any]]:
+        """List all objects of a particular type."""
+        o_list = []
+        params: Dict[str, Any] = {"limit": pagesize}
+        last_id = ""
+        while True:
+            if last_id:
+                params.update({"after": last_id})
+            obj = await self.get(url, params)
+            if not obj:
+                break
+            i_list = obj[itemtype]
+            if not i_list:
+                break
+            o_list.extend(i_list)
+            last_id = i_list[-1]["id"]
+        return o_list
